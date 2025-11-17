d31dfe7a4ba0e682c9f926f62b676b78704cef20
hungryato <hungryato17@icloud.com>
2025-11-17 17:35:52 +0900

    feat: HeaderMenu.vue에 메뉴 데이터 배열 구조 예시 추가
    - HeaderMenu.vue 파일 내에 다단계 메뉴 구성을 위한 menuList 배열 예시 작성
    - 추후 동적 메뉴 렌더링 및 기능 구현을 위한 데이터 구조 기반 마련

--- vue-template/src/components/header/HeaderMenu.vue
+++ vue-template/src/components/header/HeaderMenu.vue
@@ -2,256 +2,125 @@
   <nav class="krds-main-menu">
     <div class="inner">
       <ul class="gnb-menu">
-        <li>
-          <button type="button" class="gnb-main-trigger" data-trigger="gnb">
-            1Depth
-          </button>
-          <!-- gnb-toggle-wrap -->
-          <div class="gnb-toggle-wrap">
-            <!-- gnb-main-list -->
-            <div class="gnb-main-list" data-has-submenu="true">
-              <ul>
-                <li>
-                  <button
-                    type="button"
-                    class="gnb-sub-trigger"
-                    data-trigger="gnb"
-                  >
-                    2Depth
-                  </button>
-                  <!-- gnb-sub-list -->
-                  <div class="gnb-sub-list">
-                    <div class="gnb-sub-content">
-                      <h2 class="sub-title">
-                        2Depth title
-                        <a href="#" class="krds-btn link basic small">
-                          <span class="underline">바로가기</span>
-                          <i class="svg-icon ico-angle right"></i>
-                        </a>
-                      </h2>
-                      <ul>
-                        <li><a href="#">Last depth</a></li>
-                        <li><button type="button">Last depth</button></li>
-                      </ul>
-                    </div>
-                    <div class="gnb-sub-banner">
-                      <span class="krds-badge bg-secondary">신규 서비스</span>
-                      <button type="button" class="krds-btn medium text">
-                        메뉴명 <i class="svg-icon ico-angle right"></i>
-                      </button>
-                    </div>
-                  </div>
-                  <!-- //gnb-sub-list -->
-                </li>
-                <li>
-                  <button
-                    type="button"
-                    class="gnb-sub-trigger"
-                    data-trigger="gnb"
-                  >
-                    2Depth
-                  </button>
-                  <!-- gnb-sub-list -->
-                  <div class="gnb-sub-list between">
-                    <div class="gnb-sub-content">
-                      <h2 class="sub-title">
-                        2Depth title
-                        <a href="#" class="krds-btn link basic small">
-                          <span class="underline">바로가기</span>
-                          <i class="svg-icon ico-angle right"></i>
-                        </a>
-                      </h2>
-                      <ul>
-                        <li><a href="#">Last depth</a></li>
-                        <li><button type="button">Last depth</button></li>
-                        <li><button type="button">Last depth</button></li>
-                      </ul>
-                    </div>
-                    <div class="gnb-sub-banner">
-                      <span class="krds-badge bg-secondary">신규 서비스</span>
-                      <button type="button" class="krds-btn medium text">
-                        메뉴명 <i class="svg-icon ico-angle right"></i>
-                      </button>
-                    </div>
-                  </div>
-                  <!-- //gnb-sub-list -->
-                </li>
-                <li>
-                  <a href="#" class="gnb-sub-trigger is-link" data-trigger="gnb"
-                    >2Depth</a
-                  >
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    class="gnb-sub-trigger is-link external-link"
-                    data-trigger="gnb"
-                    target="_blank"
-                    title="새 창 열림"
-                    >2Depth</a
-                  >
-                </li>
-              </ul>
-            </div>
-            <!-- //gnb-main-list -->
-          </div>
-          <!-- //gnb-toggle-wrap -->
-        </li>
-
-        <li>
-          <button type="button" class="gnb-main-trigger" data-trigger="gnb">
-            1Depth
-          </button>
-          <!-- gnb-toggle-wrap -->
-          <div class="gnb-toggle-wrap">
-            <!-- gnb-main-list -->
-            <div class="gnb-main-list" data-has-submenu="true">
-              <ul>
-                <li>
-                  <button
-                    type="button"
-                    class="gnb-sub-trigger"
-                    data-trigger="gnb"
-                  >
-                    2Depth
-                  </button>
-                  <!-- gnb-sub-list -->
-                  <div class="gnb-sub-list">
-                    <div class="gnb-sub-content">
-                      <h2 class="sub-title"><span>2Depth title</span></h2>
-                      <ul class="type-description">
-                        <li>
-                          <h3 class="tit">
-                            <a href="#" target="_blank" title="새 창 열림"
-                              >3Depth title <i class="svg-icon ico-go"></i
-                            ></a>
-                          </h3>
-                          <p class="txt">
-                            메뉴명과 메뉴에 관한 간략한 설명이 표시되는
-                            스타일입니다.
-                          </p>
-                        </li>
-                      </ul>
-                    </div>
-                    <div class="gnb-sub-banner">
-                      <span class="krds-badge bg-secondary">신규 서비스</span>
-                      <button type="button" class="krds-btn medium text">
-                        메뉴명 <i class="svg-icon ico-angle right"></i>
-                      </button>
-                    </div>
-                  </div>
-                  <!-- //gnb-sub-list -->
-                </li>
-                <li>
-                  <button
-                    type="button"
-                    class="gnb-sub-trigger"
-                    data-trigger="gnb"
-                  >
-                    2Depth
-                  </button>
-                  <!-- gnb-sub-list -->
-                  <div class="gnb-sub-list between">
-                    <div class="gnb-sub-content">
-                      <h2 class="sub-title"><span>2Depth title</span></h2>
-                      <ul class="type-description">
-                        <li>
-                          <h3 class="tit">
-                            <a href="#" target="_blank" title="새 창 열림"
-                              >3Depth title <i class="svg-icon ico-go"></i
-                            ></a>
-                          </h3>
-                          <p class="txt">
-                            메뉴명과 메뉴에 관한 간략한 설명이 표시되는
-                            스타일입니다.
-                          </p>
-                        </li>
-                      </ul>
-                    </div>
-                    <div class="gnb-sub-banner">
-                      <span class="krds-badge bg-secondary">신규 서비스</span>
-                      <button type="button" class="krds-btn medium text">
-                        메뉴명 <i class="svg-icon ico-angle right"></i>
-                      </button>
-                    </div>
-                  </div>
-                  <!-- //gnb-sub-list -->
-                </li>
-                <li>
-                  <a href="#" class="gnb-sub-trigger is-link" data-trigger="gnb"
-                    >2Depth</a
-                  >
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    class="gnb-sub-trigger is-link external-link"
-                    data-trigger="gnb"
-                    target="_blank"
-                    title="새 창 열림"
-                    >2Depth</a
-                  >
-                </li>
-              </ul>
-            </div>
-            <!-- //gnb-main-list -->
-          </div>
-          <!-- //gnb-toggle-wrap -->
-        </li>
-
-        <li>
-          <button type="button" class="gnb-main-trigger" data-trigger="gnb">
-            1Depth
-          </button>
-          <!-- gnb-toggle-wrap -->
-          <div class="gnb-toggle-wrap">
-            <!-- gnb-main-list -->
-            <div class="gnb-main-list">
-              <!-- gnb-sub-list -->
-              <div class="gnb-sub-list single-list between">
-                <div class="gnb-sub-content">
-                  <h2 class="sub-title"><span>2Depth title</span></h2>
-                  <ul>
-                    <li><a href="#">Last depth</a></li>
-                    <li><a href="#">Last depth</a></li>
-                    <li><a href="#">Last depth</a></li>
-                    <li><a href="#">Last depth</a></li>
-                    <li><a href="#">Last depth</a></li>
-                    <li><a href="#">Last depth</a></li>
-                    <li><a href="#">Last depth</a></li>
-                    <li><a href="#">Last depth</a></li>
-                    <li><a href="#">Last depth</a></li>
-                  </ul>
-                </div>
-                <div class="gnb-sub-banner">
-                  <span class="krds-badge bg-secondary">신규 서비스</span>
-                  <button type="button" class="krds-btn medium text">
-                    메뉴명 <i class="svg-icon ico-angle right"></i>
-                  </button>
-                </div>
-              </div>
-              <!-- //gnb-sub-list -->
-            </div>
-            <!-- //gnb-main-list -->
-          </div>
-          <!-- //gnb-toggle-wrap -->
-        </li>
-
-        <li>
-          <a href="#" class="gnb-main-trigger is-link" data-trigger="gnb"
-            >링크(anchor)</a
-          >
-        </li>
-        <li>
+        <li v-for="menu in menuList" :key="menu.label">
           <button
             type="button"
-            class="gnb-main-trigger is-link"
-            data-trigger="gnb"
+            class="gnb-main-trigger"
+            :data-trigger="menu.trigger"
           >
-            링크(anchor)
+            {{ menu.label }}
           </button>
+          <!-- gnb-toggle-wrap -->
+          <div class="gnb-toggle-wrap">
+            <!-- gnb-main-list -->
+            <div class="gnb-main-list" data-has-submenu="true">
+              <ul>
+                <li v-for="itemList in menu.children" :key="itemList.label">
+                  <button
+                    type="button"
+                    class="gnb-sub-trigger"
+                    data-trigger="gnb"
+                  >
+                    {{ itemList.label }}
+                  </button>
+                  <!-- gnb-sub-list -->
+                  <div class="gnb-sub-list">
+                    <div class="gnb-sub-content">
+                      <h2 class="sub-title">{{ itemList.title }}</h2>
+                      <ul>
+                        <li v-for="item in itemList.children" :key="item.label">
+                          <a v-if="item.isLink" :href="item.href">
+                            {{ item.label }}
+                          </a>
+                          <button v-else type="button">{{ item.label }}</button>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <!-- //gnb-sub-list -->
+                </li>
+              </ul>
+            </div>
+            <!-- //gnb-main-list -->
+          </div>
+          <!-- //gnb-toggle-wrap -->
         </li>
       </ul>
     </div>
   </nav>
 </template>
+
+<script setup>
+const menuList = [
+  {
+    label: "1Depth-1",
+    trigger: "gnb",
+    children: [
+      {
+        label: "1-2Depth-1",
+        trigger: "gnb",
+        title: "1-2Depth-1 title",
+        children: [
+          { label: "1-Last depth", isLink: true, href: "#" },
+          { label: "1-Last depth", isLink: false },
+        ],
+      },
+      {
+        label: "1-2Depth-2",
+        isLink: false,
+        trigger: "gnb",
+        title: "1-2Depth-2 title",
+        children: [
+          { label: "2-Last depth", isLink: true, href: "#" },
+          { label: "2-Last depth", isLink: false },
+        ],
+      },
+      {
+        label: "1-2Depth-3",
+        isLink: false,
+        trigger: "gnb",
+        title: "1-2Depth-3 title",
+        children: [
+          { label: "3-Last depth", isLink: true, href: "#" },
+          { label: "3-Last depth", isLink: false },
+        ],
+      },
+    ],
+  },
+  {
+    label: "1Depth-2",
+    trigger: "gnb",
+    children: [
+      {
+        label: "2-2Depth-1",
+        trigger: "gnb",
+        title: "2-2Depth-1 title",
+        children: [
+          { label: "1-Last depth", isLink: true, href: "#" },
+          { label: "1-Last depth", isLink: false },
+        ],
+      },
+      {
+        label: "2-2Depth-2",
+        isLink: false,
+        trigger: "gnb",
+        title: "2-2Depth-2 title",
+        children: [
+          { label: "2-Last depth", isLink: true, href: "#" },
+          { label: "2-Last depth", isLink: false },
+        ],
+      },
+      {
+        label: "2-2Depth-3",
+        isLink: false,
+        trigger: "gnb",
+        title: "2-2Depth-3 title",
+        children: [
+          { label: "3-Last depth", isLink: true, href: "#" },
+          { label: "3-Last depth", isLink: false },
+        ],
+      },
+    ],
+  },
+];
+</script>
