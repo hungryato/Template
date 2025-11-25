3eb69465c31885cf916f828e446c1b3e9153db85
hungryato <hungryato17@gmail.com>
2025-11-25 10:55:26 +0900

    chore: index.html 타이틀 및 진입점을 main.ts로 변경

--- vue-template/src/main.ts
+++ vue-template/src/main.ts
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import '@krds.ui/vue/dist/style.css'
-import 'krds-uiux/resources/js/component/ui-script.js'
 
 createApp(App).mount('#app')
